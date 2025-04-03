7792593c11143de8f5454486879068817d515a28
Jonathan Lavén <jonathan.laven123@gmail.com>
2025-04-03 17:34:02 +0200

    update for chatgpt llm

--- Source/move/Public/llmcallcode.py
+++ Source/move/Public/llmcallcode.py
@@ -2,14 +2,15 @@ from openai import OpenAI
 from pydantic import BaseModel
 from typing import List, Dict, Union
 
-base_url = "https://api.aimlapi.com/v1"
-api_key = "490a10992e3547a8a1b254336f62c229"
+api_key = ""
 system_prompt = '''Translate necessary parameters for the functions and return in given format.,  
-the move to location function is called move with parameters x,y,z,"
+the move to location function is called move with parameters x,y,z, DET SKA ALLTID FINNAS tre parameterar (standardvärdet är X=1150 Y=490 Z=90)"
 to make the character jump, jump har inga parametrar.
+
+om spelaren ber om att få göra saker i rad. ex: gå till ett visst ställe, sen hoppa och sen gå till nästa ställe, varje del ska finnas i actions listan
 '''
 
-api = OpenAI(api_key=api_key, base_url=base_url)
+api = OpenAI(api_key=api_key)
 
 class Action(BaseModel):
     name: str
@@ -21,7 +22,7 @@ class JsonFormat(BaseModel):
 def get_ai_response(user_input: str) -> str:
     """Tar in en användarinput och returnerar svaret från OpenAI."""
     completion = api.beta.chat.completions.parse(
-        model="gpt-4o-2024-08-06",
+        model="gpt-4o-mini-2024-07-18",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_input}
